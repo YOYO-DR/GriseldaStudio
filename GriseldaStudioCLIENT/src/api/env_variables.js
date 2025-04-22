9bedf77efcb7f7661bd8fdc0111496eb091e4ee5
Yoiner Duran Rios <yoiner3216898182@gmail.com>
2025-04-22 03:13:49 -0500

    Actualizar la URL de la API en env_variables.js para incluir el sufijo '/api'

--- GriseldaStudioCLIENT/src/api/env_variables.js
+++ GriseldaStudioCLIENT/src/api/env_variables.js
@@ -3,7 +3,7 @@ const dom_react = "https://gstudio.yoyodr.dev" // ruta de la app de react princi
 const host = window.location.host;
 
 const VARIABLES = { // Le indico la ruta de la API dependiendo de si estoy en localhost o en el dominio de la app de react
-  "API_URL" : host.includes("localhost") ? "http://localhost:8000/api" : "https://gstudioapi.yoyodr.dev",
+  "API_URL" : host.includes("localhost") ? "http://localhost:8000/api" : "https://gstudioapi.yoyodr.dev/api",
 }
  // Si no estoy en localhost o en la app de react principal, entonces estoy en la app de react de pruebas, entonces le indico la ruta de la API de pruebas
 // if (!host.includes(dom_react) && !host.includes("localhost")) {
