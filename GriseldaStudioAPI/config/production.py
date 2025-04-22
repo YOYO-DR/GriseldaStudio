7dd3d0a45f6dd401c23ccbded9b30bb884fb18f5
Yoiner Duran Rios <yoiner3216898182@gmail.com>
2025-04-22 03:18:05 -0500

    Refactor configuración de producción y ajustar rutas de base de datos en docker-compose

--- GriseldaStudioAPI/config/production.py
+++ GriseldaStudioAPI/config/production.py
@@ -1,7 +1,7 @@
 import os
 
-from .settings import * #importamos todo
-from .settings import BASE_DIR # importamos la ruta de inicio
+from .settings import *  # importamos todo
+from .settings import BASE_DIR  # importamos la ruta de inicio
 import dj_database_url
 
 # se agrega los host por medio de las variables de entono que me da Azure
@@ -11,9 +11,10 @@ ALLOWED_HOSTS = [i for i in os.environ['WEBSITE_HOSTNAME'].split(
 CSRF_TRUSTED_ORIGINS = [i for i in os.environ['WEBSITE_HOSTNAME_HTTP'].split(
     ",")] if 'WEBSITE_HOSTNAME_HTTP' in os.environ else []
 
-CORS_ALLOWED_ORIGINS = [url for url in os.environ['WEBSITE_FRONTEND_URL'].split(",")] if 'WEBSITE_FRONTEND_URL' in os.environ else []
+CORS_ALLOWED_ORIGINS = [url for url in os.environ['WEBSITE_FRONTEND_URL'].split(
+    ",")] if 'WEBSITE_FRONTEND_URL' in os.environ else []
 
-#ponemos el DEBUG en false porque se va a ejecutar en produccion
+# ponemos el DEBUG en false porque se va a ejecutar en produccion
 DEBUG = False
 
 MIDDLEWARE = [
@@ -31,28 +32,31 @@ MIDDLEWARE = [
 # agregamos el storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#Pedimos la config de la base de datos 
+# Pedimos la config de la base de datos
 # conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 
 
-#extraemos los valores, y los ponemos en un diccionario para llamarlos mejor
+# extraemos los valores, y los ponemos en un diccionario para llamarlos mejor
 # conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-#Este es el valor de la variable de entono que se utiliza en Azure, puede utilizarlas independientemente en azure si quiere, lo utilizao de esta forma para solo poner una sola variable de entorno
+# Este es el valor de la variable de entono que se utiliza en Azure, puede utilizarlas independientemente en azure si quiere, lo utilizao de esta forma para solo poner una sola variable de entorno
 
-#los ponemos en la config de la base de datos
+# los ponemos en la config de la base de datos
 # Después de la configuración inicial de DATABASES
+if not os.path.exists(os.path.join(BASE_DIR, 'db')):
+  os.makedirs(os.path.join(BASE_DIR, 'db'))
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db', 'db.sqlite3')}",
         conn_max_age=600,
         conn_health_checks=True,
     )
 }
 
-#AZURE_POSTGRESQL_CONNECTIONSTRING = dbname=nombreBD host=elHost port=3306 sslmode=require user=usuario password=pass
+# AZURE_POSTGRESQL_CONNECTIONSTRING = dbname=nombreBD host=elHost port=3306 sslmode=require user=usuario password=pass
 
-#almacenmiento azure
+# almacenmiento azure
 # Configuración para el diccionario de storages
 # si estamos en producción o desarrollo, y saber de donde traer la configuración
 
